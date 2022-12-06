3e9ddc37122ce029a4605fbfe1029a0758220378
yelim <yelim785@gmail.com>
2022-12-06 12:52:24 +0900

    스크래퍼에서 장고 프로젝트와 연결 및 실행

--- hotdeal/models.py
+++ hotdeal/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
+class Deal(models.Model):
+    img_url = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    link = models.CharField(max_length=200, primary_key=True)
+    reply_count = models.IntegerField()
+    up_count = models.IntegerField()
+    cdate = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return f'{self.title}-{self.reply_count}-{self.up_count}--{self.cdate}'
